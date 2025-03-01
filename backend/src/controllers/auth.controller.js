9d856a1423e5cdcf7acd0cd313930f615e0ec1e4
MMH576 <monjorulmohammed@gmail.com>
2025-03-01 16:28:48 -0500

    added generatetoken function to controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
     const { email, fullName, password } = req.body;
@@ -23,13 +24,22 @@ export const signup = async (req, res) => {
         });
 
         if(newUser) {
-            // generate token
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                email: newUser.email,
+                fullName: newUser.fullName,
+                profilePicture: newUser.profilePicture,
+            });
         }else{
             return res.status(400).json({ message: "Invalid user data" });
         }
 
     } catch (error) {
-
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal server error" });
     }
 
 };
