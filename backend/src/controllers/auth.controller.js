6ac1d4edd113bde704dc139bcd9ae37f9ad5d8bd
MMH576 <monjorulmohammed@gmail.com>
2025-03-01 16:04:27 -0500

    started making token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,37 @@
+import bcrypt from "bcrypt";
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
-    res.send("signup route");
+    const { email, fullName, password } = req.body;
+    try {
+        if(password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+
+        const user = await User.findOne({ email });
+        if(user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({ 
+            email, 
+            fullName, 
+            password: hashedPassword 
+        });
+
+        if(newUser) {
+            // generate token
+        }else{
+            return res.status(400).json({ message: "Invalid user data" });
+        }
+
+    } catch (error) {
+
+    }
+
 };
 
 export const login = async (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.PORT;
 
