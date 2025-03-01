2d4f25b62816c7fbdee6d1cea7a91fb84c19b9cb
MMH576 <monjorulmohammed@gmail.com>
2025-03-01 15:24:18 -0500

    connected mongodb

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,7 @@ export const connectDB = async () => {
         const conn =  await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Connected to ${conn.connection.host}`);
     } catch (error) {
-        console.log(error);
+        console.log("MongoDB connection error:", error);
+    
     }
 };
