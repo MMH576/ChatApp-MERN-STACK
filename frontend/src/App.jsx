41be5334925d1a192ca0e96fd1827999fa53c8a4
MMH576 <monjorulmohammed@gmail.com>
2025-03-03 07:19:33 -0500

    configure axios.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Navbar } from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { SignUpPage } from "./pages/SignUpPage";
+import { LogInPage } from "./pages/LogInPage";
+import { SettingsPage } from "./pages/SettingsPage";
+import { ProfilePage } from "./pages/ProfilePage";
 
 export const Hello = () => {
-  return <div className="text-red-500">Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/SignUp" element={<SignUpPage />} />
+        <Route path="/Login" element={<LogInPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Hello } from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Hello />
+    <BrowserRouter>
+      <Hello />
+    </BrowserRouter>
   </StrictMode>
 );
