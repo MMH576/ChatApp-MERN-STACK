0a0d01f9f7024c717f19b07ba4b3d49029a58190
MMH576 <monjorulmohammed@gmail.com>
2025-03-03 10:35:32 -0500

    integrated toast

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { MessageSquare, User, Mail, Lock, Eye, EyeOff } from "lucide-react";
 import { Link } from "react-router-dom";
-
+import AuthImagePattern from "../components/AuthImagePattern";
+import { toast } from "react-hot-toast";
 export const SignUpPage = () => {
   console.log("SignUpPage rendered");
   const [formData, setFormData] = useState({
@@ -14,10 +15,22 @@ export const SignUpPage = () => {
 
   const { signUp, isSigningUp } = useAuthStore();
 
-  const validateForm = () => {};
+  const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
+    if (!formData.email.trim()) return toast.error("Email is required");
+    if (!/\S+@\S+\.\S+/.test(formData.email))
+      return toast.error("Invalid email format");
+    if (!formData.password) return toast.error("Password is required");
+    if (formData.password.length < 6)
+      return toast.error("Password must be at least 6 characters");
+
+    return true;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const success = validateForm();
+    if (success === true) signUp(formData);
   };
 
   return (
@@ -135,6 +148,12 @@ export const SignUpPage = () => {
           </div>
         </div>
       </div>
+      {/* right side */}
+
+      <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
     </div>
   );
 };
