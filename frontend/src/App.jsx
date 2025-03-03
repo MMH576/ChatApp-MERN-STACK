7b98c1530883206ef67c46624f99c58229c97475
MMH576 <monjorulmohammed@gmail.com>
2025-03-03 07:42:39 -0500

    added cors to backend index.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,24 @@ import { LogInPage } from "./pages/LogInPage";
 import { SettingsPage } from "./pages/SettingsPage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 export const App = () => {
-  const { authUser } = useAuthStore();
+  const { authUser, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log(authUser);
 
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/SignUp" element={<SignUpPage />} />
-        <Route path="/Login" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LogInPage />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
